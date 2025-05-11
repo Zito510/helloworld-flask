b770ce5761ce64fcde426e40f3947fa0e6191118
Zito510 <fernandezifs@gmail.com>
2025-05-11 12:41:48 +0200

    Neue Routen /about und /info hinzugefügt

--- app.py
+++ app.py
@@ -1,27 +1,26 @@
 from flask import Flask
 
-# Flask-Anwendung als App = Anwendung initialisieren
-# app = Flask(import_name=__name__,
-#             static_url_path="/static-content",
-#             static_folder="my_static_content",
-#             template_folder="my_templates")
 app = Flask(__name__)
 
-# Route definieren
-@app.route('/api')
+@app.route("/api")
 def hello_world():
     return 'Hello, World!'
 
-# weitere Route definieren
-@app.route('/hello')
+@app.route("/hello")
 def hello():
     return "<h1>Hallo</h1>"
 
-## TODO: Definiert eine Route auf den Pfad /25-01 mit einer Funktion, die "Hallo Kurs 25-01" zurückgibt
-@app.route('/25-01')
-def kurs():
-    return "<p>Hallo Kurs 25-01</p>"
+@app.route("/25-01")
+def klasse():
+    return "Grüße an die Klasse 25-01!"
 
+@app.route("/about")
+def about():
+    return "Willkommen auf der ABOUT-Seite! Hier erfährst du mehr über uns."
+
+@app.route("/info")
+def info():
+    return "Das ist eine INFO-Seite über Hunde-Salons 🐶✂️"
 
 if __name__ == "__main__":
     app.run(debug=True)
